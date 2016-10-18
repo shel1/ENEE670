1f55156e3a0ba50a5b769cee85698ae4a3a2f8d1
shel1 <shel1@umbc.edu>
2016-10-17 23:12:50 -0400

    add skeleton classes for skydiver and personal space

---
+++ code/PersonalSpace.m
@@ -0,0 +1,12 @@
+classdef PersonalSpace
+    %UNTITLED5 Defines a skydiver's personal space.
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
---
+++ code/Skydiver.m
@@ -0,0 +1,12 @@
+classdef Skydiver
+    %SKYDIVER Defines the skydiver class
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
