18a9914e9aa32eeb173885a136bc299217f59c27
shel1 <shel1@umbc.edu>
2016-10-18 21:19:21 -0400

    added PersonalSpace class added Skydiver Class added proximitySim function

---
+++ code/PersonalSpace.m
@@ -0,0 +1,12 @@
+classdef PersonalSpace
+    %PERSONALSPACE Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
---
+++ code/proximitySim.m
@@ -0,0 +1,7 @@
+function [ output_args ] = proximitySim( input_args )
+%PROXIMITYSIM Summary of this function goes here
+%   Detailed explanation goes here
+    
+
+end
+
