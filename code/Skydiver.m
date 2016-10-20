64fb93928fd1db2bc5f862123519ca667fcd035e
shel1 <shel1@umbc.edu>
2016-10-19 22:46:07 -0400

    10-18-work
    got the range between two assets.

--- code/Skydiver.m
+++ code/Skydiver.m
@@ -14,35 +14,8 @@ classdef Skydiver
     end
     
     methods
-        function [obj] = Skydiver(obj, varargin)
-            obj.uid = char(java.util.UUID.randomUUID);
-            
-        end
-        function [obj] = proximityCheck(obj)
-        %%
-%             x = obj.cLat;
-%             y = obj.cLon;
-%             z = obj.cAlt;
-            x = 0;
-            y = x;
-            z = x;
-            % start with one additional asset
-            uniqueAssets = 2;
-            
-            x2 = 4;
-            y2 = 3;
-            z2 = 3.0;
-            
-            
-            dx = x - x2;
-            dy = y - y2;
-            dz = z - z2;
-                       
-            % distance in single plane
-            dxy = sqrt((dx.^2)+(dy.^2));
-            
-            % 3 D range
-            range = sqrt((dxy.^2)+(dz.^2));
+        function [obj] = Skydiver(obj,varargin)
+            obj.uid = java.util.UUID.randomUUID;
             
         end
     end
--- code/proximitySim.m
+++ code/proximitySim.m
@@ -1,7 +1,7 @@
 function [ output_args ] = proximitySim( input_args )
 %PROXIMITYSIM Summary of this function goes here
 %   Detailed explanation goes here
-    s = Skydiver();
+    
 
 end
 
