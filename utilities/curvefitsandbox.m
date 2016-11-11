4e7bb4004a06f1980480f67ebb9353a14f68c783
shel1 <shel1@umbc.edu>
2016-11-10 22:38:34 -0500

    fix curvefit sandbox merge conflict.

--- utilities/curvefitsandbox.m
+++ utilities/curvefitsandbox.m
@@ -1,23 +1,5 @@
 
 %%
-<<<<<<< HEAD
-load('LoganData.mat');
-jump = 257;
-[ T, lat, lon, hMSL, velN, velE, velD, hAcc, vAcc, sAcc ] = extractFlysightData( LoganData, jump );
-dat = [ lat, lon, hMSL, velN, velE, velD, hAcc, vAcc, sAcc ];
-
-h=.2;
-
-output = (diff(dat(:,4:6),1,1))/h;
-%estimated acceleration
-vNp= SMA(output(:,1),25);
-vEp= SMA(output(:,2),25);
-vDp= SMA(output(:,3),25);
-
-velPrime = [vNp vEp vDp];
-exitIdx = getExit(LoganData(jump));
-
-=======
 % load('LoganData.mat');
 jump = 257;
 [ T, lat, lon, hMSL, velN, velE, velD, hAcc, vAcc, sAcc ] = extractFlysightData( LoganData, jump );
@@ -32,7 +14,6 @@ vDp= SMA(output(:,3),25);
 velPrime = [vNp vEp vDp];
 exitIdx = getExit(LoganData(jump));
 
->>>>>>> proximitySim007
 close all;
 
 [r,c] = size(lat);
@@ -51,31 +32,10 @@ outStack=[];
 
 xvals=XStart:edIdx;
 %%
-<<<<<<< HEAD
-x=1;
-
-i=startIdx+(x*inc);
-edIdx = i+(sz);
-edVIdx = i+szvalid;
-latSamp = lat(i:edIdx);
-lonSamp = lon(i:edIdx);
-altSamp = hMSL(i:edIdx);
-
-vNSamp = velN(i:edIdx);
-vESamp = velE(i:edIdx);
-vDSamp = velD(i:edIdx);
-latVSamp = lat(i:edVIdx);
-lonVSamp = lon(i:edVIdx);
-altVSamp = hMSL(i:edVIdx);
-vNVSamp = velN(i:edVIdx);
-vEVSamp = velE(i:edVIdx);
-vDVSamp = velD(i:edVIdx);
-=======
 % x=1;
 % 
 % i=startIdx+(x*inc);
 
->>>>>>> proximitySim007
 
 %%
 fig=figure;
@@ -121,38 +81,20 @@ for i = startIdx:inc:r-inc
         [lonFr, lonGOF(count)] = posFit(lonSamp,lonVSamp);
         [altFr, altGOF(count)] = posFit(altSamp,altVSamp);
 
-<<<<<<< HEAD
-%         [latFr, latGOF(count)] = posFit(SMA(latSamp,k),latVSamp);
-%         [lonFr, lonGOF(count)] = posFit(SMA(lonSamp,k),lonVSamp);
-%         [altFr, altGOF(count)] = posFit(SMA(altSamp,k),altVSamp);        
-    %     frStack = [frStack frTmp];
-    %     gofStack = [gofStack gofTmp];
-    %     outStack = [outStack outTmp'];
-
-        %plot stuff
-
-        xvals = linspace(0,60,100);
-
-
-=======
         % fits are done against the range [1 50]
         % use [51 65] to extrapolate
         xvals = linspace(51,65,20);
 
->>>>>>> proximitySim007
         latFitData = [latFitData; feval(latFr,xvals)];
         lonFitData = [lonFitData; feval(lonFr,xvals)];
         altFitData = [altFitData; feval(altFr,xvals)];
         
-<<<<<<< HEAD
-=======
         % comparison plot
         
         plot3(lonSamp,latSamp,altSamp,'bo');
         hold all;
         plot3(lonFitData,latFitData,altFitData,'r.');
         
->>>>>>> proximitySim007
 %         clf;
 % 
 %         plot(xvals,latFitData,'r+');
@@ -178,14 +120,6 @@ for i = startIdx:inc:r-inc
 %     pause(.5);
     
 end
-<<<<<<< HEAD
-
-figure;plot3(lonFitData,latFitData,altFitData,'r+');
-hold all;
-plot3(lon,lat,hMSL,'b.');
-
-grid on;
-=======
 for ct=1:length(lon)
     figure;plot3(lonFitData,latFitData,altFitData,'r+');
     hold all;
@@ -195,7 +129,6 @@ for ct=1:length(lon)
     pause(1);
 end
 
->>>>>>> proximitySim007
 % ok, got thru fitting everything, now need to set up the visual.
 % do a fit, and extrapolate a few points in front, plot the real data
 
