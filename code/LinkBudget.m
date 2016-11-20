7152c57d5bdf1f8c0c39b3503c32e8194a1fef90
fdadajr <pm44612@umbc.edu>
2016-11-20 15:25:10 -0500

    Update LinkBudget.m
    Added the wavelength and antenna pattern stuff. Matt should take a look.

--- code/LinkBudget.m
+++ code/LinkBudget.m
@@ -6,12 +6,20 @@ function [ z ] = LinkBudget(lat,lon,alt,lat2,lon2,alt2)
 %% Units
 Hz = 1.0;
 KHz = 1.0e3;
-fs = 978*Hz; % frequency
+MHz = 1e6; 
+
+[d, theta] = geoDiff(lat,lon,alt,lat2,lon2,alt2);
+
+c = 299792458; % speed of light
+freq = 978*MHz; % frequency
+lambda =c/freq; % Wavelength
+
 fb = 10^6; % bit rate 
 PTx = 20; % Transmit power at the Antenna in dBm
-AG_tx = 0; % Transmit Antenna gain  
+AG_tx = log10(sin(theta).^3);
+%AG_tx = 0; % Transmit Antenna gain  
 pl = 3; %Polarization loss
-z = 10*log10(0.3.^2./(4*pi()*geoDiff(lat,lon,alt,lat2,lon2,alt2).^2)); % Free space loss calculation
+z = 10*log10(lambda/(4*pi()*d.^2)); % Free space loss calculation
 AG_rx = 10; % Recieve Antenna gain
 Pr = PTx + AG_tx + pl + z; % Recieved power
 Rs = -93; % Receive Antenna sensitivity for 90% message success rate in dBm
