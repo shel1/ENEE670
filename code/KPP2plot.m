97ee103a0ec864519edc63e2244e2c80eb503881
jacbot <akjames1@umbc.edu>
2016-12-07 23:41:20 -0500

    Changes to Link Budget/Kpp2 plot

--- code/KPP2plot.m
+++ code/KPP2plot.m
@@ -1,47 +1,48 @@
-%Link Budget and EbNo for lat lon and alt for a given alt 0 , and 10756
-% [ z, Lm, EbNo ] = LinkBudget(0,0,0,39.7716, -74.9285, 10756)
-% This function will compute the angles for a given lat lon and alt from rData
-
-
-[ d, varargout ] = geoDiff(0,0,0,39.7716, -74.9285, 10756)
+[ d, varargout ] = geoDiff(0,0,0,39.7716, -74.9285, 1001);
 %This function calculates the distance, and angles of each of the skydivers
-%by taking a slice of rData and computing it at each iteration. 
+%by taking a slice of data in dat array and computing it at each iteration. 
     
-    [g, h] = findLocDelta(0,0,1);
-varargout= 0;% assigning a variable to varargout
-% data = zeros([ ]);
-al = 0; %assigning a variable to altitude
-rg = 0; % assigning a variable to range
-dat =zeros(35,11); %allocate the values of the altitude
+[g, h] = findLocDelta(0,0,1);
+%FINDLOCDELTA Iterates over the geoDiff2d function to find lat/lon deltas
+%  Output from this goes into checking/rejecting simulated lat/lon values
+%  if they are outside the expected range.
+
+%Assigning Initial variables
+varargout= 0;
+al = 0; 
+rg = 0; 
+
+dat =zeros(35,11); 
 figure;
-for b_range = 1:11 % Linspace for ranges
-    al = 0;
+%Loop for the altitude and range and link budget
+for b_range = 1:11 
+    al = 0; 
      
-    for alt_1 = 0:35 % allocate altitude values
-        al = al+1000; % altitude increaments of 1000
+    for alt_1 = 0:35 
+        al = al+1000; 
         [~,dat(alt_1+1,b_range),~] = LinkBudget(0,0,0,0,rg,al);
     end
-    j = b_range*ones(1,11); %assigns and stores the value range values from 1-11
-    plot(j,dat(b_range,:),'-+'); %Plotting the range against the data
+    j = b_range*ones(1,11); 
+    plot(j,dat(b_range,:),'-+'); 
     hold all
-    rg = rg +h; %calculates the ranges in increments of 1
+    rg = rg +h; 
 end
-title('Plot of Range against altitude')
-xlabel('Range/nm') % x-axis
-ylabel('altitude/*1000ft') % y-axis
+%Title
+title('Plot of Range against Altitude')
+xlabel('Range (nm)') 
+ylabel('Altitude (ft*1000)') 
 grid on
 figure
       for x= 1:10
-          plot(dat(:,x),'-+');
+          semilogy(dat(:,x),'-+');
           hold all
       end
-    legend('0','1','2','3','4','5','6','7','8','9')
-    title ('Plot of the Link Margin against Range') %Title of plot
-    xlabel('Altitude/*1000 ft') % x- axis
-    ylabel('Link Margin') % y- axis
+    legend('0 -nm','1 -nm','2 -nm','3 -nm','4 -nm','5 -nm','6 -nm','7 -nm','8 -nm','9 -nm')
+    title ('Plot of the Link Margin against Range') 
+    xlabel('Altitude (ft*1000)') 
+    ylabel('Link Margin (dB)') 
     grid on
-    %surf(j,dat(b_range))
- 
+    
 
            
        
