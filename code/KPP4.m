37d2b032bb2b4c228fa2f40ef3eaeb662b79d6e2
shel1 <shel1@umbc.edu>
2016-12-12 23:02:05 -0500

    modified:   code/KPP4.m remove sinc examples switch theta_el to show null

--- code/KPP4.m
+++ code/KPP4.m
@@ -7,7 +7,7 @@
 
 %%
 % theta in radians
-xx = -pi/2:pi/500:pi/2;
+xx = 0:pi/500:pi;
 %%
 % range values
 qq = 10:10:2000;
@@ -36,38 +36,27 @@ yl=ylabel('$$\textrm{Link Margin (dB)}$$');
 yl.Interpreter = 'latex';
 t1=title('$$\textrm{Link Margin Overview}$$');
 t1.Interpreter = 'latex';
-ylim([27 85]);
+% ylim([27 85]);
 legend('10m','100m','1km','2km');
 
 
-figure;
-% semilogy(rad2deg(xx),Lm(:,1));
-hold all;
-% semilogy(rad2deg(xx),Lm(:,10));
-% semilogy(rad2deg(xx),Lm(:,100));
-semilogy(rad2deg(xx),Lm(:,200));
-hold all;
-grid on;
-xl=xlabel('$$\theta_{El}$$');
-xl.Interpreter = 'latex';
-yl=ylabel('$$\textrm{Link Margin (dB)}$$');
-yl.Interpreter = 'latex';
-xlim([-10 10]);
-% ylim([27 80]);
-ylim([27 34]);
-t1=title('$$\textrm{Link Margin Detail}$$');
-t1.Interpreter = 'latex';
-legend('10m','100m','1km','2km');
-
-mLm = Lm(:,100)- max(Lm(:,100));
-figure;
-% semilogy(rad2deg(xx),Lm(:,1));
-hold all;
-% semilogy(rad2deg(xx),Lm(:,10));
-% semilogy(rad2deg(xx),Lm(:,100));
-% plot(rad2deg(xx),Lm(:,200));
-semilogy(rad2deg(xx),mLm);
-
-grid on;
-xlabel('\theta_{El}');
-ylabel('\textrm{Link Margin (dB)}');
+% figure;
+% % semilogy(rad2deg(xx),Lm(:,1));
+% hold all;
+% % semilogy(rad2deg(xx),Lm(:,10));
+% % semilogy(rad2deg(xx),Lm(:,100));
+% semilogy(rad2deg(xx),Lm(:,200));
+% hold all;
+% grid on;
+% xl=xlabel('$$\theta_{El}$$');
+% xl.Interpreter = 'latex';
+% yl=ylabel('$$\textrm{Link Margin (dB)}$$');
+% yl.Interpreter = 'latex';
+% % xlim([-10 10]);
+% % ylim([27 80]);
+% % ylim([27 34]);
+% t1=title('$$\textrm{Link Margin Detail}$$');
+% t1.Interpreter = 'latex';
+% legend('10m','100m','1km','2km');
+% xlabel('\theta_{El}');
+% ylabel('\textrm{Link Margin (dB)}');
